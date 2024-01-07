a841e84197fdc6b69fd78e75e952e69b162069a9
ken <wqlken@163.com>
2024-01-07 16:58:10 +0800

    取消修改基本信息

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: '小王老忘的博客',
   author: '小王老忘',
   headerTitle: '小王老忘的博客',
-  //description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -13,10 +13,10 @@ const siteMetadata = {
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'wqlken@gmail.com',
   github: 'https://github.com',
-  // twitter: 'https://twitter.com/Twitter',
-  // facebook: 'https://facebook.com',
+  twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  // linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
